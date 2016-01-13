1f4ff69534e3a06e6a1c012ebe4e87a996fcce62
pleaseyouya <631119469@qq.com>
2016-01-14 00:37:33 +0800

    test listview's divider

--- app/src/main/java/com/pleaseyouya/androidbasis/MainActivity.java
+++ app/src/main/java/com/pleaseyouya/androidbasis/MainActivity.java
@@ -2,16 +2,50 @@ package com.pleaseyouya.androidbasis;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private ListView listView;
+
+    private List<String> data = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        data.add("data1");
+        data.add("data2");
+        data.add("data3");
+        data.add("data4");
+        data.add("data5");
+        data.add("data6");
+        data.add("data7");
+        data.add("data8");
+        data.add("data9");
+        data.add("data10");
+        data.add("data11");
+        data.add("data12");
+
+        listView = (ListView)findViewById(R.id.list);
+
+        listView.addHeaderView(LayoutInflater.from(this).inflate(R.layout.list_item, null));
+        listView.addFooterView(LayoutInflater.from(this).inflate(R.layout.list_item, null));
+
+        listView.setAdapter(new Adapter());
+
     }
 
 
@@ -35,5 +69,30 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    class Adapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return data.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return data.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = LayoutInflater.from(MainActivity.this).inflate(R.layout.list_item, null);
+            TextView textView = (TextView)convertView.findViewById(R.id.text);
+            textView.setText(data.get(position));
+            return convertView;
+        }
     }
 }
